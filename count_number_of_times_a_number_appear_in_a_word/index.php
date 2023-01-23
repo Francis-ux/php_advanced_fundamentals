6a47d59573dfd475dfa59fec7837fdb654627ace
micheal <micheal@gmail.com>
2023-01-23 05:44:16 +0100

    count occurence in word

---
+++ count_number_of_times_a_number_appear_in_a_word/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+
+<body>
+    <div class="form-group">
+        <form action="count_action.php" method="post">
+            <input name="word" type="text" placeholder="Enter words here">
+            <button type="submit">Count</button>
+        </form>
+    </div>
+</body>
+
+</html>

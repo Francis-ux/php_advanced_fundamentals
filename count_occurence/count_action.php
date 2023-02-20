2b09ce7ea6a4ae62ce0edd28fa072236c6b4a5a6
micheal <micheal@gmail.com>
2023-02-20 14:22:51 +0100

    commit

--- count_occurence/count_action.php
+++ count_occurence/count_action.php
@@ -1,28 +1,17 @@
 <?php
-
-$name = $_POST['word'];
-$split = str_split($name);
-$num = count($split);
-
-function count_letters($name, $num)
+$string = 'A B C ';
+$a = str_split($string);
+function count_occurrence(array $array): array
 {
-    $count = [];
-    for ($i = 0; $i < $num; $i++) {
-        $letter = $name[$i];
-        //   array_push($count, $letter);
-        if (array_key_exists($letter, $count)) {
-            $count[$letter]++;
+    $occurrence = [];
+    foreach ($array as $key => $value) {
+        if (isset($occurrence[$value])) {
+            $occurrence[$value]++;
         } else {
-            $count[$letter] = 1;
+            $occurrence[$value] = 1;
         }
     }
-    return $count;
+    return $occurrence;
 }
-if (count_letters($name, $num)) {
-    $letter_count1 = count_letters($name, $num);
-    foreach ($letter_count1 as $letter => $count) {
-        echo $letter . ":" . $count . "\n";
-    }
-} else {
-    exit("error");
-}
+$names = [1, 2, 3, 3, 3, 2, 2];
+print_r(count_occurrence($a));
